38a2d73a43f3453d2938ee921248f4e847b8dfe6
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 07:05:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
+require_relative 'spec_helper'
+require_relative '../lib/my_all.rb'
 
+describe "#my_all?" do
+  let(:collection) { [1, 2, 3] }
+
+  it "does not call on all?" do
+    expect(collection).to_not receive(:all?)
+  end
 def my_all?(collection)
 
 end
